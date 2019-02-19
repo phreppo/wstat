3ee262ba8ac1ad47891dfed16d2d89f2de5e8bbc
mazzu <denismazzu@gmail.com>
2019-02-19 15:32:48 +0100

    refactor EquationBased module

--- src/EquationBased.hs
+++ src/EquationBased.hs
@@ -7,21 +7,7 @@ import WhileGrammar
 import State
 import Domain
 
--- equational based semantic
-
-data AtomicAssign = AtomicAssign String AExpr deriving Show
-data AtomicUnaryCond = AtomicUnaryCond BArithmeticBinOperator AExpr AExpr deriving Show
-
--- assign2atomic :: Stmt -> AtomicAssign
--- assign2atomic (Assign var expr) = AtomicAssign var expr
-
--- TODO: aggiungere conversioni per gli altri casi
-bexpr2atomic :: BExpr -> AtomicUnaryCond
-bexpr2atomic (Not expr) = bexpr2atomic expr -- TODO: must rely on De Morgan rules
--- 'new' While doesn't accept all bexpr
-
-
--- -- generate Control Flow Graph from the given syntax tree
+-- generate Control Flow Graph from the given syntax tree
 
 type Equation d = (Label, [d] -> [d], Label)
 
@@ -33,33 +19,37 @@ nextLabel = (+1)
 buildEqSingleton :: ([d] -> [d]) -> Label -> ([Equation d], Label)
 buildEqSingleton x l = ([(l, x, nextLabel l)], nextLabel l)
 
--- bad code TODO: Integer -> ([Equation *], Integer) should be a Monad
-cfg :: Domain d => Stmt -> Label -> ([Equation d], Label)
-cfg (Assign var expr) = buildEqSingleton $ assign $ AtomicAssign var expr
-cfg (Assert c) = buildEqSingleton $ cond $ bexpr2atomic c
-cfg (Skip) = buildEqSingleton id
-cfg (Seq s1 s2) = \l1 -> let (xs', l2)   = cfg s1 l1
-                             (xs'', l3)  = cfg s2 l2 in
-                             (xs' ++ xs'', l3)
-cfg (If c s1 s2) = \l1 -> let l2 = nextLabel l1
-                              (xs', l3)  = cfg s1 l2
-                              l4 = nextLabel l3
-                              (xs'', l5) = cfg s2 l4
-                              l6 = nextLabel l5 in
-                              (
-                                 (l1, cond $ bexpr2atomic c, l2):
-                                 (l1, cond $ bexpr2atomic (Not c), l3):
-                                 (l3, id, l6):
-                                 (l5, id, l6):
-                              xs' ++ xs'', l6)
-cfg (While c s) = \l1 -> let l2 = nextLabel l1
-                             l3 = nextLabel l2
-                             (xs, l4) = cfg s l3
-                             l5 = nextLabel l4 in
-                             (
-                                 (l1, id, l2):
-                                 (l2, cond $ bexpr2atomic c, l3):
-                                 (l2, cond $ bexpr2atomic (Not c), l5):
-                                 (l4, id, l2):
-                             xs, l5)
+-- cfg builder
+cfgBuilder :: Domain d => Stmt -> Label -> ([Equation d], Label)
+
+-- base cases
+cfgBuilder (Assign var expr) = buildEqSingleton $ assign $ Assign var expr
+cfgBuilder (Assert c) = buildEqSingleton $ cond $ c
+cfgBuilder (Skip) = buildEqSingleton id
+
+-- recursive cases
+cfgBuilder (Seq s1 s2) = \l1 -> let (xs', l2)   = cfgBuilder s1 l1
+                                    (xs'', l3)  = cfgBuilder s2 l2 in
+                                    (xs' ++ xs'', l3)
+
+cfgBuilder (If c s1 s2) = \l1 -> let l2         = nextLabel l1
+                                     (xs', l3)  = cfgBuilder s1 l2
+                                     l4         = nextLabel l3
+                                     (xs'', l5) = cfgBuilder s2 l4
+                                     l6         = nextLabel l5 in (
+                                        (l1, cond $ c, l2):
+                                        (l1, cond $ (Not c), l3):
+                                        (l3, id, l6):
+                                        (l5, id, l6):
+                                     xs' ++ xs'', l6)
+
+cfgBuilder (While c s) = \l1 -> let l2       = nextLabel l1
+                                    l3       = nextLabel l2
+                                    (xs, l4) = cfgBuilder s l3
+                                    l5       = nextLabel l4 in (
+                                        (l1, id, l2):
+                                        (l2, cond $ c, l3):
+                                        (l2, cond $ (Not c), l5):
+                                        (l4, id, l2):
+                                    xs, l5)
 
