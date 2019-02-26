eced10a207a6f1c77b9404d8094de4160b953516
mazzu <denismazzu@gmail.com>
2019-02-26 13:30:48 +0100

    add Equation Solving sign

---
+++ src/Semantic/Equational.hs
@@ -0,0 +1,11 @@
+module Semantic.Equational where
+
+import Domain.Domain
+import Equation.EquationList
+
+-- eqSystemResolver :: Domain d =>
+--   [Equation (F d)] -> -- Xi
+--   [Label] -> -- L
+--   Int -> -- k
+--   Power d -> -- initial State
+--   Power d
