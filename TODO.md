c52590ec0ea0eb3bb7d5d7d516ecd0f648fa9c36
phreppo <parolini.francesco.1@gmail.com>
2019-02-28 23:07:14 +0100

    concrete asd

--- TODO.md
+++ TODO.md
@@ -1,9 +1,6 @@
-    . definire interfaccia AVD
-    . definire interfaccia A(S)D
-    . definire l istanza (dato un AVD) di ASD
-    
     . calcolatore di punto fisso
     . pretty printer
     . bottom top meet join etc simbolici
     . parametrizzare I
     . unary ora non prende nessun operatore come input, in quanto c e solo la negazione -> da generalizzare
+    . rifattorizzare concrete ASD del tutto
--- src/Domain/ConcreteASD.hs
+++ src/Domain/ConcreteASD.hs
@@ -1,9 +1,53 @@
 module Domain.ConcreteASD where
 
-import Domain.ASD
-import qualified Data.Map.Lazy as Map
+import Domain.AVD as V
     
-data SD = S Int
-        | Bottom
+import Domain.ASD 
+import Data.Map
+import Semantic.Atomic
+    
+data SD b = SD (Map String b)
+          | Bottom
+          deriving (Show)
 
--- instance ASD
+instance AVD b => ASD (SD b) where
+    -- subset :: SD b -> Sb d -> Bool
+    Bottom   `subset` _      = True
+    _        `subset` Bottom = False
+    (SD x)   `subset` (SD y) =
+        all (\var -> (x ! var) `V.subset` (y ! var)) (keys x)
+
+    -- bottom :: SD b
+    bottom = Bottom
+
+    -- assign :: AtomicAssign -> SD b -> SD b
+    -- TODO: more precise way to do this, URGENT
+    assign _ Bottom = Bottom
+    assign (AtomicAssign var exp) (SD x) =
+        SD $ insert var top x  
+
+    -- cond :: AtomicCond -> SD b -> SD b
+    cond _ = id
+
+    -- meet :: SD b -> SD b -> SD b
+    meet Bottom _ = Bottom
+    meet _ Bottom = Bottom
+    meet (SD x) (SD y) 
+        | any (\var -> V.isBottom $ V.meet (x ! var) (y ! var)) (keys x) = Bottom 
+        | otherwise = SD $ fromList $ zip (keys x) (fmap (\var -> (x ! var) `V.meet` (y ! var)) (keys x))
+
+    -- join :: SD b -> SD b -> SD b
+    join Bottom y = y
+    join x Bottom = x
+    join (SD x) (SD y) =
+        SD $ fromList $ zip (keys x) (fmap (\var -> (x ! var) `V.join` (y ! var)) (keys x)) 
+
+    -- widen :: SD b -> SD b -> SD b
+    widen Bottom y = y
+    widen x Bottom = x
+    widen (SD x) (SD y) =
+        SD $ fromList $ zip (keys x) (fmap (\var -> (x ! var) `V.widen` (y ! var)) (keys x)) 
+
+    -- isBottom :: d -> Bool
+    -- isBottom v = v `subset` bottom
+    
