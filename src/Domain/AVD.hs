17e9a96232b43d192b3a1a0c7a732556234f6dfe
phreppo <parolini.francesco.1@gmail.com>
2019-02-28 20:54:56 +0100

    AVD instantiated

---
+++ src/Domain/AVD.hs
@@ -0,0 +1,31 @@
+module Domain.AVD where
+
+import WhileGrammar
+
+-- Abstract Value Domain
+-- b is a powerset of abstract values
+class AVD b where
+    -- <= relation
+    subset :: b -> b -> Bool
+    
+    top :: b
+
+    bottom :: b
+
+    cons :: Integer -> b
+
+    rand :: SignedInfiniteInteger -> SignedInfiniteInteger -> b
+
+    unary :: b -> b
+
+    binary :: b -> AArithemticBinOperator -> b  -> b
+
+    join :: b -> b -> b
+
+    meet :: b -> b -> b
+
+    widen :: b -> b -> b
+
+    isBottom :: b -> Bool
+
+    -- e possibile aggiungere le funzioni per inferire automaticamente la funzione C
