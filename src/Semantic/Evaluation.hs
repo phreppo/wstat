04df13321545ff76283e993a6644a8f49e2b5998
mazzu <denismazzu@gmail.com>
2019-03-01 22:00:12 +0100

    refactor evaluation function

--- src/Semantic/Evaluation.hs
+++ src/Semantic/Evaluation.hs
@@ -1,14 +1,17 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Semantic.Evaluation where
 
 import Domain.AVD as V
+import Domain.ASD
+import State as S
 import Data.Map
 import WhileGrammar
 import Domain.SD
 
-eval :: AVD b => AExpr -> SD b -> b
-eval _         Bottom = V.bottom
-eval (Var var) (SD x) = x ! var
-eval (IntConst c) _      = cons c
-eval (NonDet c1 c2) _ = rand c1 c2
-eval (AUnary op e) x = unary op (eval e x)
-eval (ABinary op e1 e2) x = binary op (eval e1 x) (eval e1 x)
+eval :: (State d V b, ASD (d V b), AVD b) => AExpr -> d V b -> b
+eval (Var var) = S.lookup var
+eval (IntConst c) = const $ cons c
+eval (NonDet c1 c2) = const $ rand c1 c2
+eval (AUnary op e) = unary op . eval e
+eval (ABinary op e1 e2) = \x -> binary op (eval e1 x) (eval e1 x)
