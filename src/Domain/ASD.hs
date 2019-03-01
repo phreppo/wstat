81ae33442cbd5178fe648bd067c91d9240a04e33
mazzu <denismazzu@gmail.com>
2019-03-01 19:20:07 +0100

    add more precise way to define abstraction for assignements

--- src/Domain/ASD.hs
+++ src/Domain/ASD.hs
@@ -1,6 +1,7 @@
 module Domain.ASD where
 
 import Semantic.Atomic
+import WhileGrammar (AExpr)
 
 -- Abstract State Domain
 class ASD d where
@@ -11,6 +12,8 @@ class ASD d where
     assign :: AtomicAssign -> d -> d
 
     cond :: AtomicCond -> d -> d
+
+    eval :: AExpr -> d -> d
 
     meet :: d -> d -> d
 
--- src/Domain/ConcreteASD.hs
+++ src/Domain/ConcreteASD.hs
@@ -1,11 +1,11 @@
 module Domain.ConcreteASD where
 
 import Domain.AVD as V
-    
-import Domain.ASD 
+
+import Domain.ASD
 import Data.Map
 import Semantic.Atomic
-    
+
 data SD b = SD (Map String b)
           | Bottom
           deriving (Show)
@@ -22,31 +22,33 @@ instance AVD b => ASD (SD b) where
 
     -- assign :: AtomicAssign -> SD b -> SD b
     assign _ Bottom = Bottom
-    assign (AtomicAssign var exp) (SD x) =
-        SD $ insert var top x  
+    assign (AtomicAssign var exp) (SD x)
+        | V.isBottom $ eval exp (SD x) = Bottom
+        | otherwise                  = SD $ insert var (eval exp (SD x)) x
 
     -- cond :: AtomicCond -> SD b -> SD b
     cond _ = id
 
+
+
     -- meet :: SD b -> SD b -> SD b
     meet Bottom _ = Bottom
     meet _ Bottom = Bottom
-    meet (SD x) (SD y) 
-        | any (\var -> V.isBottom $ V.meet (x ! var) (y ! var)) (keys x) = Bottom 
+    meet (SD x) (SD y)
+        | any (\var -> V.isBottom $ V.meet (x ! var) (y ! var)) (keys x) = Bottom
         | otherwise = SD $ fromList $ zip (keys x) (fmap (\var -> (x ! var) `V.meet` (y ! var)) (keys x))
 
     -- join :: SD b -> SD b -> SD b
     join Bottom y = y
     join x Bottom = x
     join (SD x) (SD y) =
-        SD $ fromList $ zip (keys x) (fmap (\var -> (x ! var) `V.join` (y ! var)) (keys x)) 
+        SD $ fromList $ zip (keys x) (fmap (\var -> (x ! var) `V.join` (y ! var)) (keys x))
 
     -- widen :: SD b -> SD b -> SD b
     widen Bottom y = y
     widen x Bottom = x
     widen (SD x) (SD y) =
-        SD $ fromList $ zip (keys x) (fmap (\var -> (x ! var) `V.widen` (y ! var)) (keys x)) 
+        SD $ fromList $ zip (keys x) (fmap (\var -> (x ! var) `V.widen` (y ! var)) (keys x))
 
     -- isBottom :: d -> Bool
     -- isBottom v = v `subset` bottom
-    
