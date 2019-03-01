6f9d07ddb555b849eb9b70b2f772a648e06bce7e
mazzu <denismazzu@gmail.com>
2019-03-01 21:17:51 +0100

    add complete lattice module

---
+++ src/CompleteLattice.hs
@@ -0,0 +1,22 @@
+module CompleteLattice where
+
+-- minimal interface for a Complete Lattice
+
+class CompleteLattice p where
+
+    subset :: p -> p -> Bool -- non comparable elements returns False
+
+    top :: p
+
+    bottom :: p
+
+    join :: p -> p -> p
+
+    meet :: p -> p -> p
+
+    widen :: p -> p -> p
+
+    -- usefull auxiliary functions
+
+    isBottom :: p -> Bool
+    isBottom v = v `subset` bottom
