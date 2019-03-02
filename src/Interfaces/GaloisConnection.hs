30a44793f1d0192cc4055a6187680e7cc0269351
mazzu <denismazzu@gmail.com>
2019-03-02 12:23:06 +0100

    remove non computable class

--- src/Interfaces/GaloisConnection.hs
+++
@@ -1,8 +0,0 @@
-module Interfaces.GaloisConnection where
-
-import Interfaces.CompleteLattice
-import WhileGrammar
-
-class CompleteLattice b => GaloisConnection b where
-
-    abstraction :: I -> b
