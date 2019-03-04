444be24eb68a449d4a1dd3bd019f91a549833556
phreppo <parolini.francesco.1@gmail.com>
2019-03-04 22:42:48 +0100

    infrastructure to try things

--- TODO.md
+++ TODO.md
@@ -1,8 +1,8 @@
 PROSSIMA VOLTA
     . dominio dei segni
     . punto fisso
-    . more precise way to do backward avd operators
 
 Non urgenti
     . bottom top meet join etc simbolici
     . test una volta implementat il dominio dei segni
+    . more precise way to do backward avd operators
--- src/Semantic/Equational.hs
+++ src/Semantic/Equational.hs
@@ -4,7 +4,11 @@ import Equation.EquationList
 import Interfaces.AbstractStateDomain
 import Interfaces.CompleteLattice
 
-systemResolver :: (Label l, ASD d) =>
-    [Equation l (d -> d)] -> [l] -> Int -> d -> d
-systemResolver _ _ 0 = const bottom
--- TODO: recursive case
+-- systemResolver :: (Label l, ASD d) =>
+    -- [Equation l (d -> d)] -> [l] -> Int  -> d
+systemResolver _ _ 0 = bottom
+-- systemResolver 
+
+-- equationProgress 1 iteration equation baseStateValue =
+
+-- equationProgress programPoint iteration equation baseStateValue =
