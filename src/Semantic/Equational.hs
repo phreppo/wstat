7af7640c9ce6ba3371e36fc5f66274d03005387d
phreppo <parolini.francesco.1@gmail.com>
2019-03-13 17:00:12 +0100

    we

--- src/Semantic/Equational.hs
+++ src/Semantic/Equational.hs
@@ -17,8 +17,9 @@ fixpoint :: (ASD d, Eq d) =>
     d -> -- this will be top
     [(Label, d)]
 fixpoint equations wideningPoints state =
-    lub [ systemResolver equations programPoints wideningPoints i state | i <- [0..] ]
+    lub [ systemResolver eq programPoints wideningPoints i state | i <- [0..] ]
     where programPoints = calculateProgramPoints equations
+          (eq, _) = equations
 
 -- rmdups :: (Ord a) => [a] -> [a]
 rmdups = foldr (\x seen -> if x `elem` seen then seen else x : seen) []
@@ -34,16 +35,29 @@ lub (x:y:xs) | (x) == (y) = x
              | otherwise  = lub (y:xs)
 
 systemResolver :: ASD d =>
-    EqList Label (d -> d) -> -- cfg
+    [Equation Label (d -> d)] -> -- cfg
     [Label] -> -- program points
     [Label] -> -- widening points
     Int -> -- nth iteration
     d ->   -- initial state
     [(Label, d)]    -- state for every program point
 systemResolver _ programPoints _ 0 initialState = [ (point, firstState point initialState) | point <- programPoints]
-systemResolver (equations, _) programPoints wideningPoints iteration initialState = 
-    -- [ | programPoint <- programPoints] 
-    [(-1,bottom)]
+systemResolver equations programPoints wideningPoints iteration initialState = 
+    [ (point, newPointStateCalculator point equations wideningPoints iteration initialState )  | point <- programPoints] 
+
+newPointStateCalculator :: ASD d => 
+    Label -> -- program point
+    [Equation Label (d -> d)] -> -- list of equations
+    [Label] -> -- widening points
+    Int -> -- nth iteration
+    d ->   -- initial state
+    d      -- new state for the point
+newPointStateCalculator 1 _ _ _ initialState = initialState
+newPointStateCalculator j equations wideningPoints i initialState 
+        -- | j `elem` wideningPoints = (recCall j) `widen` (aaaaaaaaaaaaaaaaa enterPoints)
+        | False = bottom -- TODO: widening points 
+        | otherwise = systemResolver  
+    where enterPoints = [ (initialLabel, f, finalLabel) | (initialLabel, f, finalLabel) <- equations, finalLabel == j] 
 
 firstState :: (ASD d) => Label -> d -> d
 firstState p initialState | p == 1 = initialState
@@ -57,8 +71,9 @@ firstState p initialState | p == 1 = initialState
 --------------------------------------------------------------------------------
 
 fixpointComplete equations wideningPoints state = -- returns the entire table of partial results: use for debug purposes
-    lubComplete [ systemResolver equations programPoints wideningPoints i state | i <- [0..] ]
+    lubComplete [ systemResolver eq programPoints wideningPoints i state | i <- [0..] ]
     where programPoints = calculateProgramPoints equations
+          (eq, _) = equations
 
 lubComplete (x:y:xs) | (x) == (y) = (x:[y])
                      | otherwise  = x:(lubComplete (y:xs))
