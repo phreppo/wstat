01c060556cfc2198da8134f240435eaa8cebb4da
phreppo <parolini.francesco.1@gmail.com>
2019-03-13 15:11:10 +0100

    we

--- src/Semantic/Equational.hs
+++ src/Semantic/Equational.hs
@@ -10,26 +10,32 @@ import Domain.StateDomain
 import Domain.StateDomainImplementation
 import WhileGrammar
 
-fixpoint :: (Label l, ASD d, Eq d) =>
+fixpoint :: (Label l, ASD d, Eq d, Num l) =>
     EqList l (d -> d) ->
     [l] ->
     d -> -- this will be top
     [d]
 fixpoint equations wideningPoints state =
-    (⊓) [ systemResolver equations wideningPoints i state | i <- [0..] ]
+    (⊓) [ systemResolver equations programPoints wideningPoints i state | i <- [0..] ]
+    where eq = let (listOfProgramPointsAndFunctions, _) = equations in listOfProgramPointsAndFunctions
+          programPoints = [ initialLabel | (initialLabel, function, finalLabel) <- eq ]
 
 (⊓) :: (ASD d, Eq d) => [[d]] -> [d]
 (⊓) (x:y:xs) | (x) == (y) = x
              | otherwise  = (⊓) (y:xs)
 
-systemResolver :: (Label l, ASD d) =>
-    EqList l (d -> d) -> -- cfg
+systemResolver :: (Label l, ASD d, Num l) =>
+    EqList l (d -> d) -> -- list of equations
+    [l] -> -- program points
     [l] -> -- widening points
     Int -> -- nth iteration
     d ->   -- base state
     [d]    -- state for every program point
-systemResolver _ _ 0 _ = [bottom]
-systemResolver _ _ _ _ = [bottom]
+systemResolver _ programPoints _ 0 initialState = [ firstState point initialState | point <- programPoints]
+systemResolver _ _ _ _ _ = [bottom]
+
+firstState p initialState | p == 1 = initialState
+                          | otherwise = bottom
 -- systemResolver
 
 -- equationProgress 1 iteration equation baseStateValue =
