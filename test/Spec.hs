15756ee0f8d23b618c9fae930676428d05910fb0
phreppo <parolini.francesco.1@gmail.com>
2019-03-16 16:36:55 +0100

    vscode tasks for ghci

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,20 +1,20 @@
 -- vscode + intero doesn't recognize Tasty module even stack test work
 
-import Test.Tasty
-import Test.Tasty.HUnit
+-- import Test.Tasty
+-- import Test.Tasty.HUnit
 
-import StmtParseSpec
-import WhileProgramSpec
-import NonDetSpec
+-- import StmtParseSpec
+-- import WhileProgramSpec
+-- import NonDetSpec
 -- import EquationBasedSpec
 -- import WideningPointsSpec
 
-main :: IO ()
-main = do
-  defaultMain (testGroup "Library tests" tests)
-    where
-      tests = WhileProgramSpec.tests
-              ++ StmtParseSpec.tests
-              ++ NonDetSpec.tests
+-- main :: IO ()
+-- main = do
+--   defaultMain (testGroup "Library tests" tests)
+--     where
+--       tests = WhileProgramSpec.tests
+--               ++ StmtParseSpec.tests
+--               ++ NonDetSpec.tests
               -- ++ EquationBasedSpec.tests
               -- ++ WideningPointsSpec.tests
