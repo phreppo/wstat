b2d5e71a78ecc690c0e13e8eef20717c96014137
mazzu <denismazzu@gmail.com>
2019-03-19 00:18:20 +0100

    implement subset operator

--- src/Domains/SignDomain.hs
+++ src/Domains/SignDomain.hs
@@ -3,6 +3,8 @@
 
 module Domains.SignDomain where
 
+import Interfaces.CompleteLattice
+
 --------------------------------------------------------------------------------
 --                             Sign Domain
 --------------------------------------------------------------------------------
@@ -16,3 +18,33 @@ data SignDomain = BottomSign
                 | LowerEqZero
                 | TopSign
                 deriving (Show, Read, Eq, Ord, Enum)
+
+-- SignDomain is a Complete Lattice
+instance CompleteLattice SignDomain where
+
+ -- subset GreaterEqZero    GreaterEqZero   = True
+    subset BottomSign       _               = True
+    subset _                BottomSign      = False
+    subset _                TopSign         = True
+    subset TopSign          _               = False
+
+    subset EqualZero        LowerEqZero     = True
+    subset EqualZero        GreaterEqZero   = True
+    subset EqualZero        EqualZero       = True
+    subset EqualZero        _               = False
+    subset _                EqualZero       = False
+
+    subset LowerZero        LowerEqZero     = True
+    subset LowerZero        NonZero         = True
+    subset LowerZero        LowerZero       = True
+    subset LowerZero        _               = False
+    subset _                LowerZero       = False
+
+    subset GreaterZero      GreaterEqZero   = True
+    subset GreaterZero      NonZero         = True
+    subset GreaterZero      GreaterZero     = True
+    subset GreaterZero      _               = False
+    subset _                GreaterZero     = False
+
+    subset x                y               = x == y
+
