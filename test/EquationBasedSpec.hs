b4d7ac2e8e71cf9dd08c9d4d2b95f5c059f69695
mazzu <denismazzu@gmail.com>
2019-03-18 21:23:35 +0100

    fix spec

--- test/EquationBasedSpec.hs
+++ test/EquationBasedSpec.hs
@@ -1,97 +1,72 @@
 module EquationBasedSpec -- (tests)
     where
 
--- import Test.Tasty
--- import Test.Tasty.HUnit
+import Test.Tasty
+import Test.Tasty.HUnit
 
--- import Equation.CfgBuilder
--- import Equation.EquationList
--- import WhileGrammar
--- import Domain.Domain
+import Equation.MonadicCfg
+import Equation.EquationList
+import Tool.StateTransitions
+import WhileGrammar
 
--- tests = [whilePrograms x (showCFG (buildCfg y 1) [state]) name | (x, y, name) <- zip3 expected_cases result_cases names]
-
-
--- -- fake domain -----------------------------------------------------------------
--- data Tmp = T [(String, Integer)] deriving Show
-
--- instance Eq Tmp where
---   _ == _ = True
-
--- instance State Tmp where
---   lookupState _ = id
---   updateState _ _ = id
--- instance Domain Tmp where
---   subset _ _ = False
---   isBottom _ = False
---   top = []
---   bottom = []
---   assign _ = id
---   cond _ = id
---   join _ = id
---   meet _ = id
---   widen _ = id
+tests = [whilePrograms x (applyST (cfg y (const 's') (const 'c')) 1) name | (x, y, name) <- zip3 expected_cases result_cases names]
 
 
--- state = T [("x", 0)]
--- --------------------------------------------------------------------------------
+ass_result = Assign "x" (IntConst 2)
+ass_expected = ([(1, 's', 2)], 2)
+ass_name = "assignments"
 
--- ass_result = Assign "x" (IntConst 2)
--- ass_expected = ([(1, [state], 2)], 2)
--- ass_name = "assignments"
+seq_result = Seq Skip Skip
+seq_expected = ([(1, 's', 2), (2, 's', 3)], 3)
+seq_name = "Sequencing Skips"
 
--- seq_result = Seq Skip Skip
--- seq_expected = ([(1, [state], 2), (2, [state], 3)], 3)
--- seq_name = "Sequencing Skips"
+cond_result = If (BoolConst True) Skip Skip
+cond_expected = ([
+    (1,'c',2),
+    (1,'c',4),
+    (3,'s',6),
+    (5,'s',6),
+    (2,'s',3),
+    (4,'s',5)],6)
 
--- cond_result = If (BoolConst True) Skip Skip
--- cond_expected = ([
---     (1, [state], 2),
---     (1, [], 4),
---     (3, [state], 6),
---     (5, [state], 6),
---     (2, [state], 3),
---     (4, [state], 5)
---   ], 6)
--- cond_name = "conditional operator"
+cond_name = "conditional operator"
 
--- while_result = While (BoolConst False) Skip
--- while_expected = ([
---     (1, [state], 2),
---     (2, [], 3),
---     (2, [state], 5),
---     (4, [state], 2),
---     (3, [state], 4)
---   ], 5)
--- while_name ="while loop"
+while_result = While (BoolConst False) Skip
+while_expected = ([
+    (1,'c',2),
+    (1,'c',4),
+    (3,'s',1),
+    (2,'s',3)],4)
 
--- skip_result = Skip
--- skip_expected = ([(1, [state], 2)], 2)
--- skip_name ="skip"
+while_name ="while loop"
 
--- assert_result = Assert (BoolConst True)
--- assert_expected = ([(1, [state], 2)], 2)
--- assert_name ="assert"
+skip_result = Skip
+skip_expected = ([(1, 's', 2)], 2)
+skip_name ="skip"
 
--- program_result = If (BoolConst True) (While (BoolConst False) Skip) (Seq Skip Skip)
--- program_expected = ([
---   (1,[state],2),
---   (1,[],7),
---   (6,[state],10),
---   (9,[state],10),
---   (2,[state],3),
---   (3,[],4),
---   (3,[state],6),
---   (5,[state],3),
---   (4,[state],5),
---   (7,[state],8),
---   (8,[state],9)],10)
--- program_name ="a simply program"
+assert_result = Assert (BoolConst True)
+assert_expected = ([(1, 'c', 2)], 2)
+assert_name ="assert"
 
--- whilePrograms x y name =
---   testCase ("Equation based [" ++ name ++ "]")
---     (assertEqual "" x y) -- x:expected, y:result
+program_result = If (BoolConst True) (While (BoolConst False) Skip) (Seq Skip Skip)
+program_expected = ([
+    (1,'c',2),
+    (1,'c',6),
+    (5,'s',9),
+    (8,'s',9),
+    (2,'c',3),
+    (2,'c',5),
+    (4,'s',2),
+    (3,'s',4),
+    (6,'s',7),
+    (7,'s',8)],9)
 
--- result_cases = [ass_result, seq_result, cond_result, while_result, skip_result, assert_result, program_result]
--- expected_cases = [ass_expected, seq_expected, cond_expected, while_expected, skip_expected, assert_expected, program_expected]
--- names = [ass_name, seq_name, cond_name, while_name, skip_name, assert_name, program_name]
+program_name ="a simply program"
+
+whilePrograms x y name =
+  testCase ("Equation based [" ++ name ++ "]")
+    (assertEqual "" x y) -- x:expected, y:result
+
+result_cases = [ass_result, seq_result, cond_result, while_result, skip_result, assert_result, program_result]
+expected_cases = [ass_expected, seq_expected, cond_expected, while_expected, skip_expected, assert_expected, program_expected]
+names = [ass_name, seq_name, cond_name, while_name, skip_name, assert_name, program_name]
