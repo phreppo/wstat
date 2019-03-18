2e57138a97e7e2ce437a037218034353a96a0017
mazzu <denismazzu@gmail.com>
2019-03-18 22:07:46 +0100

    f

--- src/Semantic/Atomic.hs
+++ src/Semantic/Atomic.hs
@@ -2,5 +2,8 @@ module Semantic.Atomic where
 
 import SyntacticStructure.WhileGrammar
 
+data AtomicAssign = AtomicAssign String AExpr
+                  deriving Show
+
 data AtomicCond   = AtomicCond BArithmeticBinOperator AExpr AExpr
                   deriving Show
