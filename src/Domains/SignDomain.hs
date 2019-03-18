b4f841e9306f5ff2c2f99c13724ab60877bef5e9
mazzu <denismazzu@gmail.com>
2019-03-19 00:34:42 +0100

    implemented top, bottom and join

--- src/Domains/SignDomain.hs
+++ src/Domains/SignDomain.hs
@@ -22,29 +22,69 @@ data SignDomain = BottomSign
 -- SignDomain is a Complete Lattice
 instance CompleteLattice SignDomain where
 
- -- subset GreaterEqZero    GreaterEqZero   = True
-    subset BottomSign       _               = True
-    subset _                BottomSign      = False
-    subset _                TopSign         = True
-    subset TopSign          _               = False
+    subset BottomSign  _             = True
+    subset _           BottomSign    = False
 
-    subset EqualZero        LowerEqZero     = True
-    subset EqualZero        GreaterEqZero   = True
-    subset EqualZero        EqualZero       = True
-    subset EqualZero        _               = False
-    subset _                EqualZero       = False
+    subset _           TopSign       = True
+    subset TopSign     _             = False
 
-    subset LowerZero        LowerEqZero     = True
-    subset LowerZero        NonZero         = True
-    subset LowerZero        LowerZero       = True
-    subset LowerZero        _               = False
-    subset _                LowerZero       = False
+    subset EqualZero   LowerEqZero   = True
+    subset EqualZero   GreaterEqZero = True
+    subset EqualZero   EqualZero     = True
+    subset EqualZero   _             = False
+    subset _           EqualZero     = False
 
-    subset GreaterZero      GreaterEqZero   = True
-    subset GreaterZero      NonZero         = True
-    subset GreaterZero      GreaterZero     = True
-    subset GreaterZero      _               = False
-    subset _                GreaterZero     = False
+    subset LowerZero   LowerEqZero   = True
+    subset LowerZero   NonZero       = True
+    subset LowerZero   LowerZero     = True
+    subset LowerZero   _             = False
+    subset _           LowerZero     = False
 
-    subset x                y               = x == y
+    subset GreaterZero GreaterEqZero = True
+    subset GreaterZero NonZero       = True
+    subset GreaterZero GreaterZero   = True
+    subset GreaterZero _             = False
+    subset _           GreaterZero   = False
 
+    subset x           y             = x == y
+
+    top    = TopSign
+    bottom = BottomSign
+
+    join TopSign        _               = TopSign
+    join _              TopSign         = TopSign
+    join BottomSign     x               = x
+    join x              BottomSign      = x
+
+    join NonZero        NonZero         = NonZero
+    join EqualZero      EqualZero       = EqualZero
+    join GreaterZero    GreaterZero     = GreaterZero
+    join GreaterEqZero  GreaterEqZero   = GreaterEqZero
+    join LowerZero      LowerZero       = LowerZero
+    join LowerEqZero    LowerEqZero     = LowerEqZero
+
+    join NonZero        LowerZero       = NonZero
+    join NonZero        GreaterZero     = NonZero
+
+    join LowerEqZero    LowerZero       = LowerEqZero
+    join LowerEqZero    EqualZero       = LowerEqZero
+
+    join GreaterEqZero  GreaterZero     = GreaterEqZero
+    join GreaterEqZero  EqualZero       = GreaterEqZero
+
+    join EqualZero      LowerEqZero     = LowerEqZero
+    join EqualZero      GreaterEqZero   = GreaterEqZero
+    join EqualZero      LowerZero       = LowerEqZero
+    join EqualZero      GreaterZero     = GreaterEqZero
+
+    join LowerZero      LowerEqZero     = LowerEqZero
+    join LowerZero      NonZero         = NonZero
+    join LowerZero      EqualZero       = LowerEqZero
+    join LowerZero      GreaterZero     = NonZero
+
+    join GreaterZero    GreaterEqZero   = GreaterEqZero
+    join GreaterZero    NonZero         = NonZero
+    join GreaterZero    EqualZero       = GreaterEqZero
+    join GreaterZero    LowerZero       = NonZero
+
+    join _              _               = TopSign
