fa26f7cdddfd18c284d10687de0d9a71cbd176be
mazzu <denismazzu@gmail.com>
2019-04-02 21:29:00 +0200

    remove useless dep

--- src/Semantic/EquationSolver.hs
+++ src/Semantic/EquationSolver.hs
@@ -6,22 +6,21 @@ import Domains.SimpleSignDomain
 import Interfaces.AbstractStateDomain
 import Interfaces.AbstractValueDomain
 import Interfaces.CompleteLattice
-import SyntacticStructure.ControlFlowGraph
 import SyntacticStructure.ProgramPoints
 import SyntacticStructure.WhileGrammar
 
-type ProgramPointState  st = (Label, st) 
+type ProgramPointState  st = (Label, st)
 type ProgramPointsState st = [ProgramPointState st]
 
--- finds the fixpoint of the system of equations induced by the cfg and resturns 
+-- finds the fixpoint of the system of equations induced by the cfg and resturns
 -- one abstract state for every program point.
 fixpoint :: ASD d =>
     ControlFlowGraph (d -> d) ->
     [Label] ->           -- widening points
     d ->                 -- initial state
-    ProgramPointsState d -- final result: a state for every program point  
+    ProgramPointsState d -- final result: a state for every program point
 fixpoint controlFlowGraph wideningPoints initialState =
-    lub [ systemResolver controlFlowGraph programPoints wideningPoints i initialState | i <- [0..]] 
+    lub [ systemResolver controlFlowGraph programPoints wideningPoints i initialState | i <- [0..]]
     where programPoints = buildProgramPoints controlFlowGraph
 
 -- selects the first two equal states: the fixpoint
@@ -31,20 +30,20 @@ lub (x:y:xs) | x == y    = x
 
 -- resolves the system of equations induced by the cfg at the nth iteration
 systemResolver :: ASD d =>
-    ControlFlowGraph (d -> d) -> 
+    ControlFlowGraph (d -> d) ->
     [Label] ->            -- program points
     [Label] ->            -- widening points
     Int ->                -- nth iteration
     d ->                  -- initial state
     ProgramPointsState d  -- state for every program point
 systemResolver controlFlowGraph programPoints wideningPoints iteration initialState =
-    [ (programPoint, programPointNewStateCalculator programPoint controlFlowGraph wideningPoints iteration initialState )  
+    [ (programPoint, programPointNewStateCalculator programPoint controlFlowGraph wideningPoints iteration initialState )
         | programPoint <- programPoints]
 
 -- calculates the state of one program point at the nth iteration
 programPointNewStateCalculator :: ASD d =>
     Label ->                       -- program point
-    ControlFlowGraph (d -> d) -> 
+    ControlFlowGraph (d -> d) ->
     [Label] ->                     -- widening points
     Int ->                         -- nth iteration
     d ->                           -- initial state
@@ -57,4 +56,3 @@ programPointNewStateCalculator programPoint cfg wideningPoints i initialState
     where entryProgramPoints = [ (initialLabel, f, finalLabel) | (initialLabel, f, finalLabel) <- cfg, finalLabel == programPoint]
           programPointOldAbstractState = programPointNewStateCalculator programPoint cfg wideningPoints (i-1) initialState
           programPointNewAbstractState = foldr join bottom [ f $ programPointNewStateCalculator l0 cfg wideningPoints (i-1) initialState | (l0, f, l1) <- entryProgramPoints ]
-          
