317c97d0e8dc21031872182c97b4be08de98852a
mazzu <denismazzu@gmail.com>
2019-04-05 12:41:13 +0200

    fix most precise cond operations for ssd

--- src/Domains/SimpleSignDomain.hs
+++ src/Domains/SimpleSignDomain.hs
@@ -152,6 +152,46 @@ instance ASD (SD Var SimpleSignDomain) where
         | otherwise                     = update var (abstractEval exp x) x
 
     -- cond :: AtomicCond -> SimpleSignStateDomain -> SimpleSignStateDomain
-    cond _ = id -- worst scenario
+    cond _ Bottom = Bottom
+    cond (AtomicCond IsEqual (Var var) (IntConst number)) x
+        | number == 0 = update var EqualZero x
+        | number <  0 = case abstractEval (Var var) x of
+            LowerEqZero -> x
+            _           -> Bottom
+        | otherwise   = case abstractEval (Var var) x of
+            GreaterEqZero -> x
+            _           -> Bottom
+
+    cond (AtomicCond IsNEqual (Var var) (IntConst number)) x
+        | number == 0 = case abstractEval (Var var) x of
+            EqualZero -> Bottom
+            _         -> x
+        | otherwise   = x
+
+    cond (AtomicCond LessEq (Var var) (IntConst number)) x
+        | number <  0 = case abstractEval (Var var) x of
+            LowerEqZero -> x
+            _           -> Bottom
+        | otherwise   = x
+
+    cond (AtomicCond GreaterEq (Var var) (IntConst number)) x
+        | number >  0 = case abstractEval (Var var) x of
+            GreaterEqZero -> x
+            _             -> Bottom
+        | otherwise   = x
+
+    cond (AtomicCond Less (Var var) (IntConst number)) x
+        | number <= 0 = case abstractEval (Var var) x of
+            LowerEqZero -> x
+            _           -> Bottom
+        | otherwise   = x
+
+    cond (AtomicCond Greater (Var var) (IntConst number)) x
+        | number >= 0 = case abstractEval (Var var) x of
+            GreaterEqZero -> x
+            _           -> Bottom
+        | otherwise   = x
+
+    cond (AtomicCond _ _ _) x = x -- always a sound abstraction
 
 type SimpleSignStateDomain = SD Var SimpleSignDomain
