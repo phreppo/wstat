1362652b2597758ecc19686cf1f8bdf4ddf0c3c5
mazzu <denismazzu@gmail.com>
2019-04-13 00:26:05 +0200

    implemented som basic arithmetic usefull operator

--- src/Domains/CongruenceDomain.hs
+++ src/Domains/CongruenceDomain.hs
@@ -1,3 +1,28 @@
 module Domains.CongruenceDomain where
 
 --
+
+--------------------------------------------------------------------------------
+--                             Utility Functions
+--------------------------------------------------------------------------------
+
+divides :: Integer -> Integer -> Bool
+y `divides` y' = y' `mod` y == 0
+-- y || y' = any id [y' == k * y | k <- [0..]]
+
+isCongruence :: Integer -> Integer -> Integer -> Bool
+isCongruence x x' y = y `divides` (makePositive $ x - x')
+
+makePositive :: Integer -> Integer
+makePositive x | x >= 0 = x
+               | otherwise = -x
+
+lcmWithZero :: Integer -> Integer -> Integer
+lcmWithZero 0 _  = 0
+lcmWithZero _ 0  = 0
+lcmWithZero y y' = lcm y y'
+
+gcdZeroTransparent :: Integer -> Integer -> Integer
+gcdZeroTransparent 0 y  = y
+gcdZeroTransparent y 0  = y
+gcdZeroTransparent y y' = gcd y y'
